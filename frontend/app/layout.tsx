74a2639a6453d72f19a574f89c4f068679691f2a
Visnusah <240152@softwarica.edu.np>
2026-07-28 13:54:20 +0545

    chore: move Material Symbols import to head and update tsconfig jsx setting

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -24,7 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={plusJakartaSans.variable}>
-
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
+        />
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
         <Toaster position="bottom-right" />
